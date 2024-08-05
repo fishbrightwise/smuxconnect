29981c822f52c1809562747e9fa1fdf532c6292f
fishbrightwise <147294128+fishbrightwise@users.noreply.github.com>
2024-08-05 23:48:58 +0800

    Pre-validation changes

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,9 +1,18 @@
 // Login validation
 function validateForm() {
-    window.location.href = "home.html";
+    var user = document.getElementById('username').value;
+    var pass = document.getElementById('password').value;
+    var username = "username";
+    var password = "password";
+
+    if (user === username && pass === password) {
+        // Redirect to home.html
+        window.location.replace('home.html')
+    } else {
+        alert("Login was unsuccessful, please check your username and password");
+        return false;
+    }
 }
-
-
 
 // Register validation
 var email = document.forms['regForm']['email'];

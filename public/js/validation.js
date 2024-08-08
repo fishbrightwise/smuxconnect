0b7155fbfd05cac240ada688521fcee25574db5b
fishbrightwise <147294128+fishbrightwise@users.noreply.github.com>
2024-08-08 18:20:24 +0800

    Password Encryption with Crypto.js

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,13 +1,14 @@
 // Login validation
 function validateForm() {
-    var user = document.getElementById('email').value;
-    var pass = document.getElementById('password').value;
+    let email = document.getElementById('email').value;
+    let pass = document.getElementById('password').value;
     
     axios.get('https://smux-connect-default-rtdb.asia-southeast1.firebasedatabase.app/user.json?auth=' + firebaseAPIKey.API_KEY)
     .then((response) => {
         const data = response.data;
         for (const key in data) {
-            if (data[key].email === user && data[key].password === pass) {
+            decrypted = decryptPassword(data[key].password, email);
+            if (data[key].email === email && decrypted === pass) {
                 window.sessionStorage.setItem('user', key);
                 window.sessionStorage.setItem('name', data[key].name);
                 window.location.replace('home.html');
@@ -25,37 +26,51 @@ function createUser() {
     let name = document.getElementById('name').value;
     let pass = document.getElementById('pass').value;
     let cfmpass = document.getElementById('cfmpass').value;
+    let errors = '';
 
     // Validaton
+    if (email === '' || name === '' || pass === '' || cfmpass === '') {
+        errors = 'Please fill in all fields';
+    }
+    else if (pass !== cfmpass) {
+        errors = 'Passwords do not match';
+    }
 
-    // Axios POST request
-    axios.post('https://smux-connect-default-rtdb.asia-southeast1.firebasedatabase.app/user.json?auth=' + firebaseAPIKey.API_KEY, {
-        email: email,
-        name: name,
-        password: pass,
-        connection: {'dummy': 'dummy'}
-    })
-    .then((response) => {
-        const id = response.data.name;
-        // Axios GET request for game items allocation
-        axios.get('https://smux-connect-default-rtdb.asia-southeast1.firebasedatabase.app/item.json?auth=' + firebaseAPIKey.API_KEY)
+    if (errors !== '') {
+        document.getElementById('errors').innerHTML = errors;
+        return;
+    }
+    else {
+        let encrypted = encryptPassword(pass, email)
+        // Axios POST request
+        axios.post('https://smux-connect-default-rtdb.asia-southeast1.firebasedatabase.app/user.json?auth=' + firebaseAPIKey.API_KEY, {
+            email: email,
+            name: name,
+            password: encrypted,
+            connection: {'dummy': 'dummy'}
+        })
         .then((response) => {
-            const data = response.data;
-            let temp_arr = [];
-            let temp_obj = {};
-            for (const key in data) {
-                temp_arr.push(key)
-                temp_obj[key] = 0;
-            }
-            // Assign items to user
-            axios.patch('https://smux-connect-default-rtdb.asia-southeast1.firebasedatabase.app/user/' + id + '.json?auth=' + firebaseAPIKey.API_KEY, {
-                inventory: temp_arr,
-                bingo: temp_obj
+            const id = response.data.name;
+            // Axios GET request for game items allocation
+            axios.get('https://smux-connect-default-rtdb.asia-southeast1.firebasedatabase.app/item.json?auth=' + firebaseAPIKey.API_KEY)
+            .then((response) => {
+                const data = response.data;
+                let temp_arr = [];
+                let temp_obj = {};
+                for (const key in data) {
+                    temp_arr.push(key)
+                    temp_obj[key] = 0;
+                }
+                // Assign items to user
+                axios.patch('https://smux-connect-default-rtdb.asia-southeast1.firebasedatabase.app/user/' + id + '.json?auth=' + firebaseAPIKey.API_KEY, {
+                    inventory: temp_arr,
+                    bingo: temp_obj
+                });
+                alert('Account created successfully. You will be redirected back to the login page.');
+                window.location.replace('index.html');
             });
-            alert('Account created successfully. You will be redirected back to the login page.');
-            window.location.replace('index.html');
         });
-    });
+    }
 }
 
 function logout() {
@@ -73,4 +88,14 @@ function loggedInCheck () {
     if (window.sessionStorage.getItem('user') !== null) {
         window.location.replace('home.html');
     }
+}
+
+// Password Encryption. Uses Crypto.js
+function encryptPassword(password, email) {
+    return CryptoJS.AES.encrypt(password, email).toString(); // Binds password to an identifier, the user's email
+};
+
+// Password Decryption. Uses Crypto.js
+function decryptPassword(password, email) {
+    return CryptoJS.AES.decrypt(password, email).toString(CryptoJS.enc.Utf8); // Verifies identifier, the user's email to unlock password.
 }

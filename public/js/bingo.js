58ceeeeec3546ac5f5c0f98db8c7b45a24944077
fishbrightwise <147294128+fishbrightwise@users.noreply.github.com>
2024-08-11 20:28:45 +0800

    Added completion screen for completed home.html

--- public/js/bingo.js
+++ public/js/bingo.js
@@ -2,29 +2,44 @@ async function getItems() {
     const response = await axios.get('https://smux-connect-default-rtdb.asia-southeast1.firebasedatabase.app/user/' + sessionStorage.getItem("user") + '.json?auth=' + firebaseAPIKey.API_KEY);
     let table = document.getElementById('bingo');
     let counter = 0;
-    for (let key in response.data.bingo) {
-        if (response.data.bingo[key] === 0) {
-            
-            table.innerHTML += `
-            <div class="col s6 m4 l3">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="./img/emo${response.data.stickers[counter]}.jpg">
-                    </div>
+
+    if (!response.data.inventory) {
+        document.getElementById('bingoBoard').innerHTML = `
+            <div class="card">
+                <div class="card-image" style="margin: 5% 5% 0 5%">
+                    <img src="./img/star.png" style="width:100%; height: auto">
                 </div>
-            </div>`;
-        }
-        else {
-            table.innerHTML += `
-            <div class="col s6 m4 l3">
-                <div class="card">
-                    <div class="card-image">
-                        <img src="./img/Done.png">
-                    </div>
+                <div class="card-content">
+                    <p><b>Congratulations!</b> <br>You have completed the bingo board! Keep on connecting!</p>
+                    <p>Here's a star for you!</p>
                 </div>
-            </div>`;
-        }
-        counter++;
+            </div>
+        `;
+    }
+    else {
+        for (let key in response.data.bingo) {
+            if (response.data.bingo[key] === 0) {
+                table.innerHTML += `
+                <div class="col s6 m4 l3">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="./img/emo${response.data.stickers[counter]}.jpg">
+                        </div>
+                    </div>
+                </div>`;
+            }
+            else {
+                table.innerHTML += `
+                <div class="col s6 m4 l3">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="./img/Done.png">
+                        </div>
+                    </div>
+                </div>`;
+            }
+            counter++;
+        }    
     }
 };
 
